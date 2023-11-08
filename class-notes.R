67ee57f035ce41077c61ed69b84bb597e5b25fdc
The Olsons <jessicaforsdick@jessicas-air-3.ad.uvu.edu>
2023-11-08 11:43:42 -0700

    edits for class notes

--- class-notes.R
+++ class-notes.R
@@ -4,77 +4,100 @@
 # Spring 2024
 #
 
+## Use existing datasets!
+# all data and methods should be Earth science based
+
 ### Weekly breakdown
 
-## 1. Github Basics and Rstudio setup
+### 1. Github Basics and Rstudio setup
 # discuss R, register, and discuss basics of programming
-
+#
 # task 01
 # My first script
-
-## 2. Data types, basic R methods
+#
+### 2. Data types, basic R methods
 # Intro to GitHub
 # data types: string, integer, data.frame, etc., 
 # reading in a file, plotting information
-
+#
 # task 02
 # Exploring a dataset
-data(mtcars)
-head(mrcars,3)
-class(mtcars)
-dim(mtcars)
-
+# change to iris dataset
+#
 # case study 01
 # 
-
-## 3. Better data visualizations 
+#
+### 3. Better data visualizations 
 # GitHub workflows
 # use ggplot, continue exploring data types, etc.
+# use dataset from faculty or UGRC
 
-## 4. Data wrangling
-#
-
-
+### 4. Data wrangling & statistical models
+# USGS water dataset
 # skills test 1
 
-## 5. Vector data I
+### 5. Vector data 
 # reading and visualizing vector data
+# UGRC data
+# SNOTEL Data
 # basic attribute analysis
+# look at Zahn guest lecture
 
-## 6. Vector data II
+## 6. Vector analysis
 # Spatial relationships, joins and data aggregation, choropleth mapping
+# spatial interpolation, Kriging
+# Kripa project - Precip variability & water towers
+# ?? introduce custom functions?
 
-## 7. Raster data I
-#
+### 7. Raster data 
+# terrain analysis
+# suitability analysis - avalanches - or bigfoot (maybe later?)
+# look at zahn guest lecture
 
-## 8. Raster data II
-#
+### 8. Raster analysis
+# Drosophila case study
 
 ### SPRING BREAK (data unclear) ###
 
-## 9. Project proposals and interactive mapping
-#
-
+### 9. Project proposals and interactive mapping
+# plotting with leaflet
+# creating polygons on the fly (draw etc.)
 
 # skills test 2
+# terrible maps???
 
-## 10. Remote Sensing I
+### 10. Remote Sensing I
+# basic NDVI, or classification
+# ?? case studies could all include GEE for final weeks
 
-#  case studies could all include GEE for final weeks
+### 11. Time-series datasets and temporal statistics
+# time-series modeling 
+# lag and correlation between MODIS SCA and RF
+# ?? include GEE library ??
 
-## 11. Time-series datasets
+### 12. Remote Sensing II
+# hyperspectral from Noah
 
-## 12. Remote Sensing II
-
-## 13. Creating custom functions and tools
+### 13. Creating custom functions and tools
+# could create custom tool for OLI-DRFS
 
 # Skills test 3
 
-## 14. Spatial Modeling (could be earlier?) - incorporated in vector II
+### 14. Spatial Modeling (could be earlier?) - incorporated in vector II
+# functions
+# Solar Radiation?
+# Ecological Modeling
 
-## 15-16. Project Workshops
+### 15-16. Project Workshops
 
 
+# ideas for datasets
+# glacier length
+# minimal glacier models
+# USGS water data
+# SNOTEL
+# Mountain Project climbs - with rock types - distance to?
+# 
 
 
 
