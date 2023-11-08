5f8df6222c337c7d0f62fe3a8ca81364639a73a4
The Olsons <jessicaforsdick@Jessicas-MacBook-Air-3.local>
2023-11-07 22:13:11 -0700

    create syllabus and first files

---
+++ class-notes.R
@@ -0,0 +1,87 @@
+#
+# Notes and code testing GDS course
+# GEOG-490R
+# Spring 2024
+#
+
+### Weekly breakdown
+
+## 1. Github Basics and Rstudio setup
+# discuss R, register, and discuss basics of programming
+
+# task 01
+# My first script
+
+## 2. Data types, basic R methods
+# Intro to GitHub
+# data types: string, integer, data.frame, etc., 
+# reading in a file, plotting information
+
+# task 02
+# Exploring a dataset
+data(mtcars)
+head(mrcars,3)
+class(mtcars)
+dim(mtcars)
+
+# case study 01
+# 
+
+## 3. Better data visualizations 
+# GitHub workflows
+# use ggplot, continue exploring data types, etc.
+
+## 4. Data wrangling
+#
+
+
+# skills test 1
+
+## 5. Vector data I
+# reading and visualizing vector data
+# basic attribute analysis
+
+## 6. Vector data II
+# Spatial relationships, joins and data aggregation, choropleth mapping
+
+## 7. Raster data I
+#
+
+## 8. Raster data II
+#
+
+### SPRING BREAK (data unclear) ###
+
+## 9. Project proposals and interactive mapping
+#
+
+
+# skills test 2
+
+## 10. Remote Sensing I
+
+#  case studies could all include GEE for final weeks
+
+## 11. Time-series datasets
+
+## 12. Remote Sensing II
+
+## 13. Creating custom functions and tools
+
+# Skills test 3
+
+## 14. Spatial Modeling (could be earlier?) - incorporated in vector II
+
+## 15-16. Project Workshops
+
+
+
+
+
+
+# Ideas from course:
+# https://geo511.wilsonlab.io/Syllabus.html
+# https://github.com/AdamWilsonLabEDU/GEO511
+
+
+
